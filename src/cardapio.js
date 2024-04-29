5ddddfa4570ca7daa8404764693685615d0d90fc
feliperyo <ryofeliperyo@gmail.com>
2024-04-29 09:55:07 -0300

    alterando Menu

--- src/cardapio.js
+++ src/cardapio.js
@@ -1,74 +1,144 @@
-import enroladinhoMignonImg from './assets/pratos/enroladinho-mignon.jpeg';
-import casal20Img from './assets/pratos/casal-20.jpeg';
-import burguerPicanhaImg from './assets/pratos/burguer-picanha.jpeg';
-import fishChipsImg from './assets/pratos/fish-chips.jpeg';
+import lagostaGrelhadaImg from './assets/pratos/lagosta-grelhada.jpg';
+import lagostaCocoImg from './assets/pratos/lagosta-coco.jpg';
+import polvoGrelhadoImg from './assets/pratos/polvo-grelhado.jpg';
+import polvoArrozImg from './assets/pratos/arroz-polvo.jpg';
+import CamaraoGrelhadoImg from './assets/pratos/camarao-grelhado.jpg';
 import risotoCamaraoTrufadoImg from './assets/pratos/risoto-camarao-trufado.jpeg';
+import fishChipsImg from './assets/pratos/fish-chips.jpeg';
+import iscaPeixeImg from './assets/pratos/isca-peixe.jpg';
+import moquecaImg from './assets/pratos/moqueca.jpg';
+import bacalhauImg from './assets/pratos/bacalhau.jpg';
 
-import brownieImg from './assets/sobremesas/brownie.jpeg';
-import cocadaFornoImg from './assets/sobremesas/cocada-forno.jpeg';
+
 import petitGateauImg from './assets/sobremesas/petit-gateau.jpeg';
+import brownieImg from './assets/sobremesas/brownie.jpeg';
+import sorveteNutellaImg from './assets/sobremesas/sorvete-de-nutella.jpg';
+import sorveteCafeImg from './assets/sobremesas/cafe.jpg';
+import mousseImg from './assets/sobremesas/mousse.jpg';
+import mousseMangaImg from './assets/sobremesas/manga.jpg'
+import tortaImg from './assets/sobremesas/torte.jpg'
+import cocadaFornoImg from './assets/sobremesas/cocada-forno.jpeg';
+
 
 import aguaSemGasImg from './assets/bebidas/agua-sem-gas.jpeg';
 import aguaComGasImg from './assets/bebidas/agua-com-gas.jpeg';
+import sucoImg from './assets/bebidas/suco-de-fruta.jpg';
 import cocaColaImg from './assets/bebidas/coca-cola.jpeg';
 import guaranaImg from './assets/bebidas/guarana.jpeg';
 import heinekenImg from './assets/bebidas/heineken.jpeg';
 
-export const enroladinhoMignon = {
-  nome: 'Enroladinho de Mignon',
-  preco: 'R$ 64,90',
-  imagem: enroladinhoMignonImg,
-  descricao: 'Finíssimas fatias de filé mignon enroladas no parmesão',
-};
-const casal20 = {
-  nome: 'Casal 20',
-  preco: 'R$ 29,00',
-  imagem: casal20Img,
-  descricao: 'Casal perfeito pão de alho caseiro e linguiça toscana grill',
-};
-const burguerPicanha = {
-  nome: 'Burguer de Picanha',
-  preco: 'R$ 44,90',
-  imagem: burguerPicanhaImg,
-  descricao:
-    'Burguer de Picanha Angus, Queijo Cheddar, Crisp de Cebola e Geléia de Bacon',
-};
-const fishChips = {
-  nome: 'Fish and Chips',
+const lagostaGrelhada = {
+  nome: 'Lagosta Grelhada',
   preco: 'R$ 79,90',
-  imagem: fishChipsImg,
-  descricao:
-    'Iscas de Peixe Empanada na Farinha Panko e Flocos de Milho e Batata Frita Palito',
+  imagem: lagostaGrelhadaImg,
+  descricao: 'A lagosta grelhada com manteiga de limão e ervas é um prato sofisticado e delicioso.',
+};
+const lagostaCoco = {
+  nome: 'Lagosta ao Coco',
+  preco: 'R$ 64,90',
+  imagem: lagostaCocoImg,
+  descricao: 'Deliciosa lagosta grelhada com purê de mandioquinha e espuma de coco.',
+};
+const polvoGrelhado = {
+  nome: 'Polvo Grelhado',
+  preco: 'R$ 94,90',
+  imagem: polvoGrelhadoImg,
+  descricao: 'Polvo grelhado com legumes é nossa especialidade.',
+};
+const polvoArroz = {
+  nome: 'Risoto de Polvo',
+  preco: 'R$ 79,90',
+  imagem: polvoArrozImg,
+  descricao: 'Risoto de Polvo com Açafrão, Parmesão, Tomate Cereja e Dill.',
+};
+const CamaraoGrelhado = {
+  nome: 'Camarão Grelhado',
+  preco: 'R$ 42,90',
+  imagem: CamaraoGrelhadoImg,
+  descricao: 'O camarão grelhado é um prato simples e delicioso.',
 };
 const risotoCamaraoTrufado = {
   nome: 'Risoto de Camarão Trufado',
   preco: 'R$ 72,90',
   imagem: risotoCamaraoTrufadoImg,
-  descricao:
-    'Arroz Arbóreo, camarões salteados no azeite trufado e crocantes de panko',
+  descricao: 'Arroz Arbóreo, camarões salteados no azeite trufado e crocantes de panko.',
+};
+const fishChips = {
+  nome: 'Fish and Chips',
+  preco: 'R$ 79,90',
+  imagem: fishChipsImg,
+  descricao: 'Iscas de Peixe Empanada na Farinha Panko e Flocos de Milho e Batata Frita Palito.',
+};
+const iscaPeixe = {
+  nome: 'Isca de Peixe',
+  preco: 'R$ 59,90',
+  imagem: iscaPeixeImg,
+  descricao: 'Iscas de Peixe Empanada na Farinha Panko.',
+};
+const moqueca = {
+  nome: 'Moqueca',
+  preco: 'R$ 119,90',
+  imagem: moquecaImg,
+  descricao: 'Preparado com cação, robalo, badejo e camarão. Aquele tempero caprichado não podem faltar!',
+};
+const bacalhau = {
+  nome: 'Bacalhoada',
+  preco: 'R$ 119,90',
+  imagem: bacalhauImg,
+  descricao: 'Bacalhau dessalgado, azeite, cebola, alho, tomate, batatas, pimentão e azeitonas.',
 };
 
+
+const petitGateau = {
+  nome: 'Petit Gateau',
+  preco: 'R$ 34,90',
+  imagem: petitGateauImg,
+  descricao: 'Bolo quente recheado com ganache de chocolate, leite condensado e castanha de caju.',
+};
 const brownie = {
   nome: 'Brownie',
   preco: 'R$ 32,90',
   imagem: brownieImg,
-  descricao:
-    'Delicioso brownie caseiro, sorvete de creme, calda quente de chocolate',
+  descricao: 'Delicioso brownie caseiro, sorvete de creme, calda quente de chocolate.',
+};
+const sorveteNutella = {
+  nome: 'Sorvete de Nutella',
+  preco: 'R$ 24,90',
+  imagem: sorveteNutellaImg,
+  descricao: 'Sorvete com pedados e calda de Nutella.',
+};
+const sorveteCafe = {
+  nome: 'Sorvete de Café',
+  preco: 'R$ 22,90',
+  imagem: sorveteCafeImg,
+  descricao: 'Sorvete de Café com Leite e raspas de chocolate.',
+};
+const mousse = {
+  nome: 'Mousse de Morango Crocante',
+  preco: 'R$ 32,90',
+  imagem: mousseImg,
+  descricao: 'Mousse de Morango com pedaços acompanhado de Negresco.',
+};
+const mousseManga = {
+  nome: 'Mousse de Manga ao Leite',
+  preco: 'R$ 32,90',
+  imagem: mousseMangaImg,
+  descricao: 'Prove essa deliciosa novidade.',
+};
+const torta = {
+  nome: 'Torta de Limão',
+  preco: 'R$ 14,90',
+  imagem: tortaImg,
+  descricao: 'Deliciosa fatia de Torta de Limão com raspas e borda crocante.',
 };
 const cocadaForno = {
   nome: 'Cocada de Forno',
   preco: 'R$ 32,90',
   imagem: cocadaFornoImg,
-  descricao:
-    'Cremosa cocada de coco torrado, doce de leite argentino e sorvete de creme',
+  descricao: 'Cremosa cocada de coco torrado, doce de leite argentino e sorvete de creme',
 };
-const petitGateau = {
-  nome: 'Petit Gateau',
-  preco: 'R$ 34,90',
-  imagem: petitGateauImg,
-  descricao:
-    'Bolo quente recheado com ganache de chocolate, leite condensado e castanha de caju',
-};
+
+
 
 const aguaSemGas = {
   nome: 'Água sem gás',
@@ -81,6 +151,12 @@ const aguaComGas = {
   preco: 'R$ 7,90',
   imagem: aguaComGasImg,
   descricao: '310ml',
+};
+const suco = {
+  nome: 'Suco de Fruta',
+  preco: 'R$ 12,90',
+  imagem: sucoImg,
+  descricao: '500ml',
 };
 const cocaCola = {
   nome: 'Coca-Cola',
@@ -102,13 +178,34 @@ const heineken = {
 };
 
 export const pratosPrincipais = [
-  burguerPicanha,
-  enroladinhoMignon,
-  casal20,
-  fishChips,
+  lagostaGrelhada,
+  lagostaCoco,
+  polvoGrelhado,
+  polvoArroz,
+  CamaraoGrelhado,
   risotoCamaraoTrufado,
+  fishChips,
+  iscaPeixe,
+  moqueca,
+  bacalhau
 ];
 
-export const sobremesas = [brownie, cocadaForno, petitGateau];
+export const sobremesas = [
+  petitGateau,
+  brownie,
+  sorveteNutella,
+  sorveteCafe,
+  mousse,
+  mousseManga,
+  torta,
+  cocadaForno
+];
 
-export const bebidas = [aguaSemGas, aguaComGas, cocaCola, guarana, heineken];
+export const bebidas = [
+  aguaSemGas,
+  aguaComGas,
+  suco,
+  cocaCola,
+  guarana,
+  heineken
+];
